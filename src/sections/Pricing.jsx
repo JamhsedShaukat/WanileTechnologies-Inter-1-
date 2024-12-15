59351287587d061a0bf40ef0df05714b15ca88c8
JamshedShaukat <168329984+JamhsedShaukat@users.noreply.github.com>
2024-12-15 16:51:05 +0500

    installing react-scroll dependencies

--- src/sections/Pricing.jsx
+++ src/sections/Pricing.jsx
@@ -1,39 +1,3 @@
-// import clsx from "clsx";
-// import { useState } from "react";
-// import { Element } from "react-scroll";
-
-// const Pricing = () => {
-//     const [monthely,setMonthely]=useState(true);
-//   return (
-//     <section>
-//       <Element name="pricing">  
-//         <div className="container">
-//             <div className="max-w-960 pricing-head_before relative mx-auto border-l
-//              border-r border-s2 bg-s1/50 pb-40 pt-28 
-//              max-xl:max-w-4xl max-lg:border-none max-md:pb-32 max-md:pt-16">
-//                 <h3 className=" h3 max-lg:h4 max-md:h5 z-3 relative
-//                  mx-auto mb-14 max-w-lg text-center text-p4
-//                   max-md:mb-11 max:sm:max-w-sm ">
-//                    Lorem ipsum dolor, sit amet consectetur adipisicing.
-//                 </h3>
-//                 <div className="relative z-4 mx-auto flex w-[375px] rounded-3xl border-[3px] border-s4/25 
-//                  bg-s1/50 p-2 backdrop-blur-[6px] max-md:w-[310px]">
-//                     <button className={clsx("pricing-head_btn",monthely && "text-p4")} onClick={()=>setMonthely(true)}>
-//                         Monthely
-//                     </button>
-//                     <button className={clsx("pricing-head_btn" ,!monthely && "text-p4")} onClick={()=>setMonthely(false)}>
-//                        Annual
-//                     </button>
-//                 </div>
-//             </div>
-
-//         </div>
-//       </Element>
-//     </section>
-//   );
-// };
-
-// export default Pricing;
 
 import { Element } from "react-scroll";
 import { useState } from "react";
